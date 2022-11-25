ea29f9b6317fcb774d98e68b86da4f5250120724
eriawan <eriawan@live.com>
2022-11-26 03:41:55 +0700

    ensure run benchmark on collection iteration

--- src/SampleBenchmarkDotnet/AdvancedSampleBenchmarks/Program.cs
+++ src/SampleBenchmarkDotnet/AdvancedSampleBenchmarks/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace AdvancedSampleBenchmarks
+﻿using BenchmarkDotNet.Running;
+
+namespace AdvancedSampleBenchmarks
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            BenchmarkRunner.Run<CollectionIteration>();
         }
     }
 }
